5a6c0bbbd8f830cd82f14205fe97fbb425ef3386
Brendon DSouza <brendonmufc@gmail.com>
2022-06-02 00:24:46 -0400

    initial commit

---
+++ sales-by-match.js
@@ -0,0 +1,59 @@
+/* TITLE
+Sales by Match
+
+URL
+https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+INSTRUCTIONS
+There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+Example
+
+
+There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+Function Description
+
+Complete the sockMerchant function in the editor below.
+
+sockMerchant has the following parameter(s):
+
+int n: the number of socks in the pile
+int ar[n]: the colors of each sock
+Returns
+
+int: the number of pairs
+Input Format
+
+The first line contains an integer , the number of socks represented in .
+The second line contains  space-separated integers, , the colors of the socks in the pile.
+
+Constraints
+
+ where 
+Sample Input
+
+STDIN                       Function
+-----                       --------
+9                           n = 9
+10 20 20 10 10 30 50 10 20  ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+Sample Output
+
+3
+
+*/
+
+function sockMerchant(n, ar) {
+  let count = 0
+  
+  let newArr = ar.sort((a,b) => a-b)
+  for(let i = 0; i < newArr.length -1; ){
+      if(newArr[i] === newArr[i+1]) {
+          count++
+          i+=2
+      } else {
+          i++
+      }
+  }
+return count
+}
