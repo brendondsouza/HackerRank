dfc568117154bef971cbaaed5f1580ff9237cfbf
Brendon DSouza <brendonmufc@gmail.com>
2022-06-15 01:45:28 -0400

    initial commit

---
+++ sales-by-match copy.js
@@ -0,0 +1,59 @@
+/* TITLE
+Mark and toys
+
+URL
+https://www.hackerrank.com/challenges/mark-and-toys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+
+Mark and Jane are very happy after having their first child. Their son loves toys, so Mark wants to buy some. There are a number of different toys lying in front of him, tagged with their prices. Mark has only a certain amount to spend, and he wants to maximize the number of toys he buys with this money. Given a list of toy prices and an amount to spend, determine the maximum number of gifts he can buy.
+
+Note Each toy can be purchased only once.
+
+Example
+
+
+The budget is  units of currency. He can buy items that cost  for , or  for  units. The maximum is  items.
+
+Function Description
+
+Complete the function maximumToys in the editor below.
+
+maximumToys has the following parameter(s):
+
+int prices[n]: the toy prices
+int k: Mark's budget
+Returns
+
+int: the maximum number of toys
+Input Format
+
+The first line contains two integers,  and , the number of priced toys and the amount Mark has to spend.
+The next line contains  space-separated integers 
+
+Constraints
+
+/*
+ * Complete the 'maximumToys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY prices
+ *  2. INTEGER k
+ */
+
+function maximumToys(prices, k) {
+    let currCost = 0
+    let numToys = 0
+    
+    prices.sort((a,b) => a-b)
+    
+    for(let i = 0; i < prices.length; i++){
+        currCost += prices[i]
+        if(currCost > k){
+            return numToys
+        } 
+        numToys++
+    } 
+    return numToys
+
+}
